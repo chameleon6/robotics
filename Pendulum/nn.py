5e8e6917bfd2a8c9916558821bc38029e40987be
Jeffrey Yan <chameleon6@gmail.com>
2016-02-07 13:04:32 -0500

    generic nn training

--- Pendulum/nn.py
+++ Pendulum/nn.py
@@ -9,32 +9,33 @@ def s_const_grid(s):
 
 class ControlNN:
     def __init__(self):
-        tf_random_seed = 3
+        tf_random_seed = 20
+        nonlinearity = tf.nn.relu
         self.n_s = 2
         self.n_a = 1
         self.n_sa = 3
-        self.n_1 = 10
-        self.n_2 = 10
+        self.n_1 = 100
+        self.n_2 = 100
         self.sa_learn = tf.placeholder("float", shape=[None,self.n_sa])
         self.W_sa_1 = tf.Variable(tf.random_normal([self.n_sa, self.n_1], seed=tf_random_seed))
         self.W_1_2 = tf.Variable(tf.random_normal([self.n_1, self.n_2], seed=tf_random_seed))
         self.W_2_q = tf.Variable(tf.random_normal([self.n_2,1], seed=tf_random_seed))
-        self.q_learn = tf.matmul(tf.nn.relu(tf.matmul(tf.nn.relu(tf.matmul(self.sa_learn, self.W_sa_1)),
+        self.q_learn = tf.matmul(nonlinearity(tf.matmul(nonlinearity(tf.matmul(self.sa_learn, self.W_sa_1)),
             self.W_1_2)), self.W_2_q)
+        self.y_learn = tf.placeholder("float", shape = [None])
+        learn_error = tf.reduce_mean(tf.square(self.y_learn - self.q_learn))
+        self.learn_opt = tf.train.AdamOptimizer(0.001).minimize(learn_error)
 
         self.s_query = tf.placeholder("float", shape=[1, self.n_s])
         self.a_query = tf.Variable(tf.random_normal([1, self.n_a], seed=tf_random_seed))
         self.sa_query = tf.concat(1, [self.s_query, self.a_query])
-        self.q_query = tf.matmul(tf.nn.relu(tf.matmul(tf.nn.relu(tf.matmul(self.sa_query, self.W_sa_1)),
+        self.q_query = tf.matmul(nonlinearity(tf.matmul(nonlinearity(tf.matmul(self.sa_query, self.W_sa_1)),
             self.W_1_2)), self.W_2_q)
 
-        query_opt = tf.train.AdamOptimizer(1.0) # adam
+        query_opt = tf.train.AdamOptimizer(1.0)
         query_grads_and_vars = query_opt.compute_gradients(self.q_query, [self.a_query])
-
-        # grads_and_vars is a list of tuples (gradient, variable).
+        # list of tuples (gradient, variable).
         query_grads_and_vars[0] = (-query_grads_and_vars[0][0], query_grads_and_vars[0][1])
-
-        # Ask the optimizer to apply the capped gradients.
         self.apply_query_grads = query_opt.apply_gradients(query_grads_and_vars)
 
         self.init_op = tf.initialize_all_variables()
@@ -60,3 +61,6 @@ class ControlNN:
             #self.query_opt.run(feed_dict={self.s_query: s})
 
         return self.sess.run(self.a_query)
+
+    def train(self, sa_vals, y_vals):
+        self.sess.run(self.learn_opt, feed_dict={self.y_learn: y_vals, self.sa_learn: sa_vals})
--- Pendulum/nn_test.py
+++ Pendulum/nn_test.py
@@ -1,6 +1,18 @@
 from nn import *
 
 net = ControlNN()
-s = np.array([-100,30])
-print net.get_best_a(s)
+
+s = np.array([0.5,0.5])
+mb_size = 10
+num_mb = 1000
+xs = np.random.uniform(-100,100, (mb_size*num_mb, 3))
+ys = np.sum(xs,1)
+
+#net.graph_output(s)
+for i in range(num_mb):
+    print i
+    sample_x, sample_y = xs[mb_size*i:mb_size*(i+1)], ys[mb_size*i:mb_size*(i+1)]
+    net.train(sample_x, sample_y)
+
+#print net.get_best_a(s)
 net.graph_output(s)

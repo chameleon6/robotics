b41f4e7e0cfc6bc9359717f3c7197a0f918223b4
Jeffrey Yan <chameleon6@gmail.com>
2016-02-09 11:37:49 -0500

    saving model functionality

--- Pendulum/nn.py
+++ Pendulum/nn.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 def s_const_grid(s):
-    n = 1000
+    n = 10000
     xs = np.linspace(-1,1,n)[:,np.newaxis]
     return xs, np.concatenate((np.ones((n,1)) * s[np.newaxis,:], xs), 1)
 
@@ -13,14 +13,14 @@ class ControlNN:
         nonlinearity = tf.nn.relu
         self.keep_prob_train_val = 1.0
 
-        def unif_fanin_mat(shape):
+        def unif_fanin_mat(shape, name):
             b = np.sqrt(3 * self.keep_prob_train_val / shape[0])
             initial = tf.random_uniform(shape, minval=-b, maxval=b, seed=tf_random_seed)
-            return tf.Variable(initial)
+            return tf.Variable(initial, name=name)
 
-        def bias(shape):
+        def bias(shape, name):
             initial = tf.constant(0.1, shape=shape)
-            return tf.Variable(initial)
+            return tf.Variable(initial, name=name)
 
         self.n_s = 2
         self.n_a = 1
@@ -29,14 +29,18 @@ class ControlNN:
         self.n_1 = n_hidden
         self.n_2 = n_hidden
         self.one_layer_only = True
+
         self.keep_prob = tf.placeholder('float')
         self.sa_learn = tf.placeholder('float', shape=[None,self.n_sa])
-        self.W_sa_1 = unif_fanin_mat([self.n_sa, self.n_1])
-        self.b_1 = bias([self.n_1])
-        self.W_1_2 = unif_fanin_mat([self.n_1, self.n_2])
-        self.b_2 = bias([self.n_2])
-        self.W_2_q = unif_fanin_mat([self.n_2,1])
-        self.b_q = bias([1])
+        self.W_sa_1 = unif_fanin_mat([self.n_sa, self.n_1], 'W_sa_1')
+        self.b_1 = bias([self.n_1], 'b_1')
+        self.W_1_2 = unif_fanin_mat([self.n_1, self.n_2], 'W_1_2')
+        self.b_2 = bias([self.n_2], 'b_2')
+        self.W_2_q = unif_fanin_mat([self.n_2,1], 'W_2_q')
+        self.b_q = bias([1], 'b_q')
+        name_var_pairs = zip(['W_sa_1', 'b_1', 'W_1_2', 'b_2', 'W_2_q', 'b_q'],
+                [self.W_sa_1, self.b_1, self.W_1_2, self.b_2, self.W_2_q, self.b_q])
+        self.name_var_dict = {i:j for (i,j) in name_var_pairs}
 
         def q_from_input(i):
             o1 = tf.nn.dropout(nonlinearity(tf.matmul(i, self.W_sa_1) + self.b_1), self.keep_prob)
@@ -53,7 +57,7 @@ class ControlNN:
         self.learn_opt = tf.train.AdamOptimizer(0.001).minimize(self.learn_error)
 
         self.s_query = tf.placeholder('float', shape=[1, self.n_s])
-        self.a_query = unif_fanin_mat([1, self.n_a])
+        self.a_query = unif_fanin_mat([1, self.n_a], 'a_query')
         self.sa_query = tf.concat(1, [self.s_query, self.a_query])
         self.q_query = q_from_input(self.sa_query)
 
@@ -63,14 +67,20 @@ class ControlNN:
         query_grads_and_vars[0] = (-query_grads_and_vars[0][0], query_grads_and_vars[0][1])
         self.apply_query_grads = query_opt.apply_gradients(query_grads_and_vars)
 
-        self.init_op = tf.initialize_all_variables()
+        self.save_path = '/tmp/model.ckpt'
+        self.saver = tf.train.Saver(self.name_var_dict)
+        self.use_saved = True
 
         self.sess = tf.Session()
+
+        self.init_op = tf.initialize_all_variables()
         self.sess.run(self.init_op)
 
+        if self.use_saved:
+            self.load_model()
+
     def print_params(self):
-        for (name, param) in zip(['W_sa_1', 'b_1', 'W_1_2', 'b_2', 'W_2_q', 'b_q'],
-                [self.W_sa_1, self.b_1, self.W_1_2, self.b_2, self.W_2_q, self.b_q]):
+        for (name, param) in enumerate(self.name_var_dict):
             print name
             print self.sess.run(param)
         print
@@ -105,3 +115,9 @@ class ControlNN:
     def train(self, sa_vals, y_vals):
         self.sess.run(self.learn_opt, feed_dict={self.y_learn: y_vals,
             self.sa_learn: sa_vals, self.keep_prob: self.keep_prob_train_val})
+
+    def save_model(self):
+        self.saver.save(self.sess, self.save_path)
+
+    def load_model(self):
+        self.saver.restore(self.sess, self.save_path)
--- Pendulum/nn_test.py
+++ Pendulum/nn_test.py
@@ -6,38 +6,23 @@ start_time = time.time()
 net = ControlNN()
 print "compile time", time.time() - start_time
 
-s = np.array([1,1])
+s = np.array([0.5, 0.5])
 mb_size = 20
-num_mb = 5000
+num_mb = 3000
 n_test = 1000
 
 def ys_from_xs(xs):
     #return np.sum(xs,1)[:,np.newaxis]
-    ys = xs[:,0]**2 + xs[:,1] + xs[:,2]**2
+    ys = -(xs[:,0]**2 + xs[:,1] + (xs[:,2] - 0.5)**2)
     return ys[:, np.newaxis]
 
-xs = np.random.uniform(-1,1, (mb_size*num_mb, 3))
+xs = np.random.uniform(-5,5, (mb_size*num_mb, 3))
 ys = ys_from_xs(xs)
 
-xs_test = np.random.uniform(-1,1, (n_test, 3))
+xs_test = np.random.uniform(-5,5, (n_test, 3))
 ys_test = ys_from_xs(xs_test)
 
-#net.graph_output(s)
-
 start_time = time.time()
-
-# print xs[0:2], ys[0:2]
-# for i in range(1000):
-#     net.train(xs[0:2], ys[0:2])
-#     if i%100 == 0:
-#         num_eval = 2
-#         net_out = net.q_from_sa(xs[0:num_eval])
-#         print net_out, ys[0:num_eval]
-#         print np.mean((net_out - ys[0:num_eval])**2), net.mse_q(xs[0:num_eval], ys[0:num_eval])
-#         print net.o1_from_sa(xs[0:num_eval])
-#         net.print_params()
-#
-# sys.exit()
 
 for i in range(num_mb):
     sample_x, sample_y = xs[mb_size*i:mb_size*(i+1)], ys[mb_size*i:mb_size*(i+1)]
@@ -47,6 +32,7 @@ for i in range(num_mb):
     if i%1000 == 0:
         print i, net.mse_q(xs_test, ys_test), net.mse_q(sample_x, sample_y)
 
+net.save_model()
 print "train time", time.time() - start_time
 
 print net.get_best_a(s)

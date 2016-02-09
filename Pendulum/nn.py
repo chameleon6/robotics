34114f7f28bc9ac0ead79c7d45933768008b76cb
Jeffrey Yan <chameleon6@gmail.com>
2016-02-09 01:05:49 -0500

    Trying to get NN linear prediction...

--- Pendulum/nn.py
+++ Pendulum/nn.py
@@ -9,9 +9,9 @@ def s_const_grid(s):
 
 class ControlNN:
     def __init__(self):
-        tf_random_seed = 4
+        tf_random_seed = 6
         nonlinearity = tf.nn.relu
-        self.keep_prob_train_val = 0.5
+        self.keep_prob_train_val = 1.0
 
         def unif_fanin_mat(shape):
             b = np.sqrt(3 * self.keep_prob_train_val / shape[0])
@@ -25,10 +25,12 @@ class ControlNN:
         self.n_s = 2
         self.n_a = 1
         self.n_sa = 3
-        self.n_1 = 20
-        self.n_2 = 20
-        self.keep_prob = tf.placeholder("float")
-        self.sa_learn = tf.placeholder("float", shape=[None,self.n_sa])
+        n_hidden = 5
+        self.n_1 = n_hidden
+        self.n_2 = n_hidden
+        self.one_layer_only = True
+        self.keep_prob = tf.placeholder('float')
+        self.sa_learn = tf.placeholder('float', shape=[None,self.n_sa])
         self.W_sa_1 = unif_fanin_mat([self.n_sa, self.n_1])
         self.b_1 = bias([self.n_1])
         self.W_1_2 = unif_fanin_mat([self.n_1, self.n_2])
@@ -38,15 +40,18 @@ class ControlNN:
 
         def q_from_input(i):
             o1 = tf.nn.dropout(nonlinearity(tf.matmul(i, self.W_sa_1) + self.b_1), self.keep_prob)
+            if self.one_layer_only:
+                return tf.matmul(o1, self.W_2_q) + self.b_q
+
             o2 = tf.nn.dropout(nonlinearity(tf.matmul(o1, self.W_1_2) + self.b_2), self.keep_prob)
             return tf.matmul(o2, self.W_2_q) + self.b_q
 
         self.q_learn = q_from_input(self.sa_learn)
-        self.y_learn = tf.placeholder("float", shape = [None])
-        learn_error = tf.reduce_mean(tf.square(self.y_learn - self.q_learn))
-        self.learn_opt = tf.train.AdamOptimizer(0.001).minimize(learn_error)
+        self.y_learn = tf.placeholder('float', shape = [None])
+        self.learn_error = tf.reduce_mean(tf.square(self.y_learn - self.q_learn))
+        self.learn_opt = tf.train.AdamOptimizer().minimize(self.learn_error)
 
-        self.s_query = tf.placeholder("float", shape=[1, self.n_s])
+        self.s_query = tf.placeholder('float', shape=[1, self.n_s])
         self.a_query = unif_fanin_mat([1, self.n_a])
         self.sa_query = tf.concat(1, [self.s_query, self.a_query])
         self.q_query = q_from_input(self.sa_query)
@@ -62,17 +67,24 @@ class ControlNN:
         self.sess = tf.Session()
         self.sess.run(self.init_op)
 
+    def print_params(self):
+        for (name, param) in zip(['W_sa_1', 'b_1', 'W_1_2', 'b_2', 'W_2_q', 'b_q'],
+                [self.W_sa_1, self.b_1, self.W_1_2, self.b_2, self.W_2_q, self.b_q]):
+            print name
+            print self.sess.run(param)
+
+    def q_from_sa(self, sa_vals):
+        return self.sess.run(self.q_learn, feed_dict={self.sa_learn: sa_vals, self.keep_prob: 1.0})
+
     def graph_output(self, s):
         assert self.n_a == 1
         xs, inputs = s_const_grid(s)
-        outputs = self.sess.run(self.q_learn, feed_dict={self.sa_learn: inputs, self.keep_prob: 1.0})
+        outputs = self.q_from_sa(inputs)
         plt.plot(xs, outputs)
         plt.show()
         return outputs
 
     def get_best_a(self, s):
-        # Compute the gradients for a list of variables.
-
         for i in range(100):
             #if i%10 == 0:
             #    print i
@@ -81,5 +93,10 @@ class ControlNN:
 
         return self.sess.run(self.a_query)
 
+    def mse_q(self, sa_vals, y_vals):
+        return self.sess.run(self.learn_error,
+                feed_dict={self.sa_learn: sa_vals, self.y_learn: y_vals, self.keep_prob: 1.0})
+
     def train(self, sa_vals, y_vals):
-        self.sess.run(self.learn_opt, feed_dict={self.y_learn: y_vals, self.sa_learn: sa_vals, self.keep_prob: self.keep_prob_train_val})
+        self.sess.run(self.learn_opt, feed_dict={self.y_learn: y_vals,
+            self.sa_learn: sa_vals, self.keep_prob: self.keep_prob_train_val})
--- Pendulum/nn_test.py
+++ Pendulum/nn_test.py
@@ -1,19 +1,30 @@
 from nn import *
+import time
 
+start_time = time.time()
 net = ControlNN()
+print "compile time", time.time() - start_time
 
 s = np.array([0,0])
-mb_size = 30
-num_mb = 1000
+mb_size = 20
+num_mb = 10000
+n_test = 1000
 xs = np.random.uniform(-1,1, (mb_size*num_mb, 3))
 ys = np.sum(xs,1)
 
-net.graph_output(s)
+xs_test = np.random.uniform(-1,1, (n_test, 3))
+ys_test = np.sum(xs_test,1)
+
+#net.graph_output(s)
+
+start_time = time.time()
 for i in range(num_mb):
     if i%1000 == 0:
-        print i
+        print i, net.mse_q(xs_test, ys_test)
     sample_x, sample_y = xs[mb_size*i:mb_size*(i+1)], ys[mb_size*i:mb_size*(i+1)]
     net.train(sample_x, sample_y)
+
+print "train time", time.time() - start_time
 
 print net.get_best_a(s)
 net.graph_output(s)
